cb8b6a1aea7d075824a75fa610b8773ecd1058b4
SallyThink <SallyThink@mail.ru>
2016-05-26 00:19:03 +0300

    debug form

--- resources/views/post/select.blade.php
+++ resources/views/post/select.blade.php
@@ -12,6 +12,18 @@
              {{ $post->text }}
          </div>
      </div></div>
+         <div class="col-lg-4"></div>
      </div>
  @endforeach
+    <div class="row">
+        <div class="col-lg-6 col-lg-push-3">
+    <form role="form" method="post" action="{{url ('post\/')}}{{ $post->id }}">
+        <div class="form-group">
+            <label>Your Comment</label>
+            <textarea class="form-control" rows="3"></textarea>
+        </div>
+        <input type="hidden" name="_token" value="{{{ csrf_token() }}}">
+        <button type="submit" class="btn btn-default">Comment It</button>
+    </form>
+        </div></div>
 @endsection
